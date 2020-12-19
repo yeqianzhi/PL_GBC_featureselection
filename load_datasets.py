d98edf830764e7253423198824eafed521703d32
yeqianzhi <1024455890@qq.com>
2020-12-19 20:27:07 +0800

    加载数据集

--- load_datasets.py
+++ load_datasets.py
@@ -2,27 +2,33 @@
 from scipy.io import loadmat
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-import GBNRS
+
 
 
 
 def load_datasets():
     # 获取数据集
-    m = loadmat("F:\\partial_datasets\\BirdSong.mat")
-    data = m["data"]
+    dir = "F:\\partial_datasets\\BirdSong.mat"
+    m = loadmat(dir)
+    data_no_label = m["data"]
+    # print(type(data_no_label))
+    # print(m["partial_target"])
     partial_target = m["partial_target"].todense().T
+    # partial_target = m["partial_target"].T
+    
     target = m["target"].todense().T
+    # target = m["target"].T
+
+    num_data, num_attribute = data_no_label.shape
 
     # 归一化
     minMax = MinMaxScaler()  # Normalize data
-    data = minMax.fit_transform(data)
-    return data, partial_target, target
+    data_no_label = np.hstack((minMax.fit_transform(data_no_label), data_no_label[:, 0].reshape(num_data, 1)))
+    return data_no_label, partial_target, target
 
-    for i in range(10):
-        attributes = GBNRS.get_attribute_reduction(data[:, :])
-        Attributes.append(attributes)    
-    
-    return data, partial_target, target
+
+
+
 
 
 
